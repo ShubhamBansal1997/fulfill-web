e30183254637d94c29e7799659a7eff4460ec3aa
Shubham Bansal <shubhambansal17@hotmail.com>
2020-09-29 13:48:58 +0530

    ◀️  removed the ProductFile model Not needed, handling the uploading tasks externally on the frontend

--- fulfill/product/models.py
+++ fulfill/product/models.py
@@ -1,11 +1,11 @@
 # Third Party Stuff
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-# third party
-from uuid_upload_path import upload_to
 
 # fulfill Stuff
 from fulfill.base.models import TimeStampedUUIDModel
+
+# third party
 
 
 # Create your models here.
@@ -28,20 +28,3 @@ class Product(TimeStampedUUIDModel):
 
     def __str__(self):
         return str(self.sku)
-
-
-class ProductFile(TimeStampedUUIDModel):
-    """
-    Product file model
-    """
-    task_id = models.CharField(null=True, max_length=100)
-    file = models.FileField(upload_to=upload_to, null=True)
-    status = models.CharField(null=False, default='ADDED', max_length=20)
-
-    class Meta:
-        verbose_name = _("product file")
-        verbose_name_plural = _("product files")
-        ordering = ("-created_at",)
-
-    def __str__(self):
-        return str(self.id)
