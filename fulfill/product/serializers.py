d974844d9163a848ef6f4a5c46e9974ffd35585a
Shubham Bansal <shubhambansal17@hotmail.com>
2020-10-03 10:09:51 +0530

    ✨ webhooks api

--- fulfill/product/serializers.py
+++ fulfill/product/serializers.py
@@ -1,7 +1,11 @@
 # Third Party Stuff
-from rest_framework import serializers
+from django.conf import settings
+from rest_framework import exceptions, serializers
+from rest_hooks.models import Hook
 
 from .models import Product
+
+HOOK_EVENTS = getattr(settings, 'HOOK_EVENTS', [])
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -19,3 +23,20 @@ class ProductFileSerializer(serializers.Serializer):
     serializes the data of the product upload api
     """
     filename = serializers.CharField(required=True)
+
+
+class HookSerializer(serializers.ModelSerializer):
+    """
+    serializes the hook data
+    """
+
+    def validate_event(self, event):
+        if event not in HOOK_EVENTS:
+            err_msg = "Unexpected event {}".format(event)
+            raise exceptions.ValidationError(detail=err_msg, code=400)
+        return event
+
+    class Meta:
+        model = Hook
+        fields = '__all__'
+        read_only_fields = ('user',)
