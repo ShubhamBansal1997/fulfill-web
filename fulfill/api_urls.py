51a4a070b729b09635fc1328b3f3eeff6c8483d8
Shubham Bansal <shubhambansal17@hotmail.com>
2020-10-03 10:07:32 +0530

    ✨ added the webhook urls to the drf

--- fulfill/api_urls.py
+++ fulfill/api_urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 # fulfill Stuff
 from fulfill.base.api.routers import SingletonRouter
-from fulfill.product.api import ProductViewSet, UploadProductViewSet
+from fulfill.product.api import HookViewSet, ProductViewSet, UploadProductViewSet
 from fulfill.users.api import CurrentUserViewSet
 from fulfill.users.auth.api import AuthViewSet
 
@@ -15,7 +15,7 @@ default_router.register("auth", AuthViewSet, basename="auth")
 singleton_router.register("me", CurrentUserViewSet, basename="me")
 default_router.register("product", ProductViewSet, basename="product")
 default_router.register("file_upload", UploadProductViewSet, basename="uploadfile")
-
+default_router.register('webhook', HookViewSet, basename="webhook")
 
 # Combine urls from both default and singleton routers and expose as
 # 'urlpatterns' which django can pick up from this module.
