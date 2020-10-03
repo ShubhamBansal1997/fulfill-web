464feab1b37debc1b38dff66ee6bdffcf35ad5ee
Shubham Bansal <shubhambansal17@hotmail.com>
2020-10-03 10:05:38 +0530

    ⚡️ moving the webhook task to the celery worker

--- fulfill/product/tasks.py
+++ fulfill/product/tasks.py
@@ -7,6 +7,7 @@ from celery import shared_task
 from celery_progress.websockets.backend import WebSocketProgressRecorder
 from django.conf import settings
 from django.core.files.storage import default_storage
+from rest_hooks.tasks import DeliverHook
 
 # fulfill Stuff
 from fulfill.product.services import add_or_update_bulk_product
@@ -32,3 +33,15 @@ def product_upload_task(self, file):
     except Exception as e:
         logging.error(f"Error while running the task {e}")
     return result
+
+
+def deliver_hook_wrapper(target, payload, instance, hook):
+    # instance is None if using custom event, not built-in
+    if instance is not None:
+        instance_id = instance.id
+    else:
+        instance_id = None
+    # pass ID's not objects because using pickle for objects is a bad thing
+    kwargs = dict(target=target, payload=payload,
+                  instance_id=instance_id, hook_id=hook.id)
+    DeliverHook.apply_async(kwargs=kwargs)
